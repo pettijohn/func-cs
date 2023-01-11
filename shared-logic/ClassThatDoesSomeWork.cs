50ad14b982dcfd4a686edb22531cc468baafaa6b
pettijohn <pettijohn@github>
2023-01-11 10:47:20 -0800

    Stubbed logic that can target net6.0 and net7.0

---
+++ shared-logic/ClassThatDoesSomeWork.cs
@@ -0,0 +1,36 @@
+﻿namespace shared_logic;
+
+
+
+
+public class Dimensions
+{
+    public double Height { get; set; }
+    public double Width { get; set; }
+    public double Depth { get; set; }
+}
+
+public static class ClassThatDoesSomeWork
+{
+    public static string DoTheWork()
+    {
+#if NET6_0
+        return "Hello from .NET 6.0";
+#endif
+
+#if NET7_0
+        return "Hello from .NET 7.0";
+#endif
+        // Create an object
+
+        // Serialize it to JSON
+
+        // Generate an AES Key
+
+        // Encrypt the JSON
+
+        // Encode as Base64
+
+        // Return string
+    }
+}
---
+++ shared-logic/Program.cs
@@ -0,0 +1,9 @@
+namespace shared_logic;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(ClassThatDoesSomeWork.DoTheWork());
+    }
+}
