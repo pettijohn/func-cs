bd151a42cf310d44b21361f59ede5b99be417730
pettijohn <pettijohn@github>
2023-01-11 10:59:19 -0800

    Checkpoint - serialize to json works

--- shared-logic/ClassThatDoesSomeWork.cs
+++ shared-logic/ClassThatDoesSomeWork.cs
@@ -1,30 +1,40 @@
-﻿namespace shared_logic;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
 
-
-
+namespace shared_logic;
 
 public class Dimensions
 {
+    [JsonConstructorAttribute]
+    public Dimensions() { }
     public double Height { get; set; }
     public double Width { get; set; }
     public double Depth { get; set; }
 }
 
+#if NET7_0
+[JsonSourceGenerationOptions(WriteIndented = true)]
+[JsonSerializable(typeof(Dimensions))]
+internal partial class SourceGenerationContext : JsonSerializerContext
+{
+}
+#endif
+
 public static class ClassThatDoesSomeWork
 {
     public static string DoTheWork()
     {
-#if NET6_0
-        return "Hello from .NET 6.0";
-#endif
-
-#if NET7_0
-        return "Hello from .NET 7.0";
-#endif
         // Create an object
+        var dims = new Dimensions() { Height = 4.5, Width = 6.7, Depth = 8.9 };
 
         // Serialize it to JSON
-
+#if NET7_0
+        var json_dims = JsonSerializer.Serialize<Dimensions>(dims, new JsonSerializerOptions() { TypeInfoResolver = SourceGenerationContext.Default });
+#endif
+#if NET6_0
+        var json_dims = JsonSerializer.Serialize<Dimensions>(dims);
+#endif
         // Generate an AES Key
 
         // Encrypt the JSON
@@ -32,5 +42,13 @@ public static class ClassThatDoesSomeWork
         // Encode as Base64
 
         // Return string
+
+#if NET6_0
+        return "Hello from .NET 6.0" + json_dims;
+#endif
+
+#if NET7_0
+        return "Hello from .NET 7.0" + json_dims;
+#endif
     }
 }
